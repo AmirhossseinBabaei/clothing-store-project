e4dc146fe294039a75c5274a5de7505410cd7510
AmirhossseinBabaei <ab8622822@gmail.com>
2026-01-13 08:50:04 +0330

    FrontEnd Commit: Creating an get data from api method file and show in the slider of home page

--- FrontEnd/src/api/getAllSliders.jsx
+++ FrontEnd/src/api/getAllSliders.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import axios from "axios";
 
-export async function getAllSliders() {
+export async function GetAllSliders() {
     try {
-        const response = await axios.get("http://127.0.0.1:8000/api/v1/dashboard/sliders");
+        const response = await axios.get("http://127.0.0.1:8000/api/v1/sliders");
                 
         if (response.data && response.data.data) {
             return response.data.data;
@@ -15,7 +16,7 @@ export async function getAllSliders() {
             return [];
         }
     } catch (error) {
-        console.error("خطا در دریافت محصولات:", error);
+        console.error("خطا در دریافت اسلایدر ها:", error);
         throw error; 
     }
 }
--- FrontEnd/src/components/HeroSlider.jsx
+++ FrontEnd/src/components/HeroSlider.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -8,8 +9,27 @@ import img2 from "../assets/imgaes/slider/2.jpg";
 import img3 from "../assets/imgaes/slider/3.jpg";
 
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import { GetAllSliders } from "../api/GetAllSliders";
 
 function Slider() {
+
+      const [products, setProducts] = useState([]);
+      const [loading, setLoading] = useState(true);
+      const [error, setError] = useState(null);
+    
+      useEffect(() => {
+        GetAllSliders()
+          .then((data) => {
+            setProducts(data);
+            setLoading(false);
+          })
+          .catch((err) => {
+            console.error("خطا در دریافت محصولات:", err);
+            setError("خطا در دریافت محصولات");
+            setLoading(false);
+          });
+      }, []); 
+
     return (
         <>
             <div className="w-full">
@@ -23,39 +43,20 @@ function Slider() {
                     loop={true} 
                     className="rounded-2xl shadow-2xl overflow-hidden" 
                 > 
-                    <SwiperSlide> 
+                {products.map((product)=>(
+      <SwiperSlide> 
                         <div className="relative">
-                            <img src={img1} alt="Slide 1" className="w-full h-[600px] object-cover" />
+                            <img src={ product.image_src || img1} alt={product.image_alt || 'There is no alt'} className="w-full h-[600px] object-cover" />
                             <div className="absolute inset-0 bg-gradient-to-r from-black/50 to-transparent flex items-center">
                                 <div className="text-white px-12">
-                                    <h2 className="text-5xl font-bold mb-4">فروشگاه لباس شیک</h2>
-                                    <p className="text-xl mb-6">بهترین و شیک‌ترین لباس‌ها را از ما بخرید</p>
+                                    <h2 className="text-5xl font-bold mb-4">{ product.title }</h2>
+                                    <p className="text-xl mb-6">{ product.descrption }</p>
                                 </div>
                             </div>
                         </div>
                     </SwiperSlide> 
-                    <SwiperSlide> 
-                        <div className="relative">
-                            <img src={img2} alt="Slide 2" className="w-full h-[600px] object-cover" />
-                            <div className="absolute inset-0 bg-gradient-to-r from-black/50 to-transparent flex items-center">
-                                <div className="text-white px-12">
-                                    <h2 className="text-5xl font-bold mb-4">مد روز</h2>
-                                    <p className="text-xl mb-6">آخرین ترندهای مد را دنبال کنید</p>
-                                </div>
-                            </div>
-                        </div>
-                    </SwiperSlide> 
-                    <SwiperSlide> 
-                        <div className="relative">
-                            <img src={img3} alt="Slide 3" className="w-full h-[600px] object-cover" />
-                            <div className="absolute inset-0 bg-gradient-to-r from-black/50 to-transparent flex items-center">
-                                <div className="text-white px-12">
-                                    <h2 className="text-5xl font-bold mb-4">تخفیف‌های ویژه</h2>
-                                    <p className="text-xl mb-6">فرصت را از دست ندهید</p>
-                                </div>
-                            </div>
-                        </div>
-                    </SwiperSlide> 
+                ))}
+  
                 </Swiper> 
             </div>
         </>
